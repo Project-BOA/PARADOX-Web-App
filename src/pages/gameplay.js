d4fc4665ad602629bde46a23db18cd9a674ee48a
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2023-03-04 17:48:52 +0000

    move configuration

--- src/pages/gameplay.js
+++ src/pages/gameplay.js
@@ -2,20 +2,9 @@ import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, getDownloadURL, listAll } from "firebase/storage";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  apiKey: "AIzaSyCRlQyWruO-HhPLIXLbUwe8Re3WCuli_VU",
-  authDomain: "codedatabase-819d6.firebaseapp.com",
-  databaseURL: "https://codedatabase-819d6-default-rtdb.firebaseio.com",
-  projectId: "codedatabase-819d6",
-  storageBucket: "codedatabase-819d6.appspot.com",
-  messagingSenderId: "420075124138",
-  appId: "1:420075124138:web:0020e12a55853d65cf67d3",
-  measurementId: "G-QYDJZW417T",
-};
+var config = require("../modules/config.js");
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(config.firebase);
 const storage = getStorage(app);
 
 const listRef = ref(storage, "RG23");
