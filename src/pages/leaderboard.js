bf84d9d750617b6b9c32403d7020396d17c37d47
Alan O'Regan <alanjohnoregan@gmail.com>
2023-03-05 22:35:54 +0000

    fix leaderboard

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -4,7 +4,7 @@ import { Container, Card, Row, Text, Col } from "@nextui-org/react";
 import { Spacer, Link } from "@nextui-org/react";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, remove, get } from "firebase/database";
-import React, { useState } from "react";
+import React from "react";
 import { useList, useListKeys } from "react-firebase-hooks/database";
 import { useRouter } from "next/router";
 
@@ -15,20 +15,54 @@ const db = getDatabase(app);
 
 export default function Leaderboard() {
   const router = useRouter();
+  const { roomID } = router.query;
 
-  const [snapshots, loading, error] = useList(
-    ref(db, "room/" + router.query.roomID + "/leaderboard")
-  );
-
-  //   if (process.browser) {
-  //     if (roomID == undefined) {
-  //       router.push("/app");
-  //     }
-  //   }
+  function Leaderboard() {
+    const [snapshots, loading, error] = useList(
+      ref(db, "room/" + roomID + "/leaderboard")
+    );
+    return (
+      <>
+        {error && <strong>Error: {error}</strong>}
+        {loading && (
+          <Text h2 size={30} align="center">
+            Loading Leaderboard...
+          </Text>
+        )}
+        {!loading && snapshots && (
+          <React.Fragment>
+            <Text h2 size={30} align="center">
+              Leaderboard
+            </Text>
+            <Spacer y={2.5} />
+            {snapshots
+              .sort((a, b) => {
+                if (a.val() < b.val()) return 1;
+                if (a.val() > b.val()) return -1;
+                return 0;
+              })
+              .map((snap, index) => {
+                var name = snap.key;
+                var position = index + 1;
+                var score = snap.val();
+                return (
+                  <React.Fragment key={name}>
+                    <span>
+                      {position}. {name} - {score} points
+                    </span>
+                    <Spacer y={2.5} />
+                  </React.Fragment>
+                );
+              })}
+          </React.Fragment>
+        )}
+      </>
+    );
+  }
 
   async function endRoom() {
     const data = {
-      roomId: router.query.roomID,
+      roomId: roomID,
     };
 
     const options = {
@@ -57,7 +91,7 @@ export default function Leaderboard() {
             <Card css={{ $$cardColor: "#CC083E" }}>
               <Card.Body>
                 <Text h1 size={60} css={{ m: 0 }} weight="bold" align="center">
-                  {"Room ID: " + router.query.roomID}
+                  {"Room ID: " + roomID}
                 </Text>
               </Card.Body>
             </Card>
@@ -71,40 +105,7 @@ export default function Leaderboard() {
           <Col>
             <Card css={{ $$cardColor: "#00764F" }}>
               <Card.Body>
-                {error && <strong>Error: {error}</strong>}
-                {loading && (
-                  <Text h2 size={30} align="center">
-                    Loading Leaderboard...
-                  </Text>
-                )}
-                {!loading && snapshots && (
-                  <React.Fragment>
-                    <Text h2 size={30} align="center">
-                      Leaderboard
-                    </Text>
-                    <Spacer y={2.5} />
-                    {snapshots
-                      .sort((a, b) => {
-                        if (a.val() < b.val()) return 1;
-                        if (a.val() > b.val()) return -1;
-                        return 0;
-                      })
-                      .map((snap, index) => {
-                        var name = snap.key;
-                        var position = index + 1;
-                        var score = snap.val();
-                        return (
-                          <React.Fragment key={name}>
-                            <span>
-                              {position}. {name} - {score} points
-                            </span>
-                            <Spacer y={2.5} />
-                          </React.Fragment>
-                        );
-                      })}
-                  </React.Fragment>
-                )}
-
+                <Leaderboard />
                 <Button
                   onPress={(event) => {
                     endRoom();
