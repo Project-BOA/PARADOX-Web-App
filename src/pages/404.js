a9c43603a0417024f65f6f6dc624570b41ff78c6
OlaAni <b00134084@mytudublin.ie>
2023-03-20 18:44:14 +0000

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import { NextUIProvider, Link, Text } from "@nextui-org/react";
+
+export default function Custom404() {
+  return (
+    <NextUIProvider>
+      <Text h1 align="center" size={50} color="black" css={{ m: 0 }}>
+        Oops! You seem to be lost.
+      </Text>
+      <Text h1 align="center" size={50} css={{ m: 0 }}>
+        Return to home Page
+        <Link align="center" block href="/app">
+          Home
+        </Link>
+      </Text>
+    </NextUIProvider>
+  );
+}
