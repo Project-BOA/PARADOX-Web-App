e31ec1810e69e2561781b8c1ad96e4523505d23b
OlaAni <b00134084@mytudublin.ie>
2023-03-24 22:31:29 +0000

    Update gameplay.js

--- src/pages/gameplay.js
+++ src/pages/gameplay.js
@@ -17,54 +17,60 @@ var config = require("@/modules/config.js");
 const app = initializeApp(config.firebase);
 const db = getDatabase(app);
 const storage = getStorage(app);
-var fireImage = [];
 
 var time = 5;
 var getPoints = 0;
 var decrement = 0;
 
 async function getPuzzle(puzzleID) {
-  const listRef = ref(storage, puzzleID);
-
-  listAll(listRef)
-    .then((res) => {
-      res.items.forEach((itemRef) => {
-        // All the items under listRef.
-        //console.log(itemRef + "\n");
-        getImageRef(itemRef);
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        status: "ERROR",
-      });
-      console.error(error);
-    });
+  // const listRef = ref(storage, puzzleID);
+  // listAll(listRef)
+  //   .then((res) => {
+  //     res.items.forEach((itemRef) => {
+  //       // All the items under listRef.
+  //       //console.log(itemRef + "\n");
+  //       getImageRef(itemRef);
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     res.status(500).json({
+  //       status: "ERROR",
+  //     });
+  //     console.error(error);
+  //   });
 }
 
+// async function nextSlide(nextRef) {
+//   getDownloadURL(nextRef).then((url) => {
+//     const img = document.getElementById("myimg");
+//     img.setAttribute("src", url);
+//   });
+// }
 async function nextSlide(nextRef) {
-  getDownloadURL(nextRef).then((url) => {
+  await getDownloadURL(nextRef).then((url) => {
     const img = document.getElementById("myimg");
     img.setAttribute("src", url);
   });
 }
 
-function getImageRef(imageRef) {
-  fireImage.push(imageRef);
-  console.log(fireImage.length);
-}
-
+// function getImageRef(imageRef) {
+//   fireImage.push(imageRef);
+//   console.log(fireImage.length);
+// }
 export default function Gameplay({
   time,
   getPoints,
   puzzleType,
   decrement,
   puzzleID,
+  fireImage,
+  newI,
 }) {
-  var i = 0;
+  //var i = 0;
   const router = useRouter();
   var roomID = router.query.roomID;
 
+  var i = newI;
   getPuzzle(puzzleID);
   return (
     <NextUIProvider>
@@ -75,21 +81,21 @@ export default function Gameplay({
             src="/image/Loading_icon.gif"
             id="myimg"
             alt={"Puzzle image: " + fireImage[i]}
-            width="500"
-            height="500"
+            width="100%"
+            height="auto"
             object-fit="cover"
           ></Image>
         </Grid>
         <Grid xs={4}>
-          {" "}
           <CountdownCircleTimer
             isPlaying
             duration={time}
             colors={["#000C66", "#F7B801", "#A30000"]}
             colorsTime={[2, 1, 0]}
             onComplete={() => {
-              if (i >= fireImage.length / 2) {
+              if (i >= fireImage.length) {
                 router.push("/leaderboard?roomID=" + roomID);
+                i = 0;
                 return { shouldRepeat: false }; // repeat animation in 1.5 seconds
               }
               if (puzzleType == "time") {
@@ -135,35 +141,42 @@ export async function getServerSideProps(context) {
     (snapshot) => {
       if (snapshot.exists()) {
         var pieceTime = snapshot.toJSON();
-        time = pieceTime.interval;
+        time = pieceTime.interval ?? 10;
+        decrement = pieceTime.decrement ?? 10;
       } else {
-        time = 10;
         console.log("No puzzle piece available");
       }
     }
   );
 
-  if (puzzleType == "time") {
-    await get(ref_database(db, "puzzle/" + puzzleID + "/pieceTime")).then(
-      (snapshot) => {
-        if (snapshot.exists()) {
-          var pieceTime = snapshot.toJSON();
-          decrement = pieceTime.decrement;
-        } else {
-          decrement = 10;
-          console.log("No puzzle piece available");
-        }
-      }
-    );
-
-    await get(
-      ref_database(db, "room/" + context.query.roomID + "/points")
-    ).then((snapshot) => {
+  await get(ref_database(db, "room/" + context.query.roomID + "/points")).then(
+    (snapshot) => {
       getPoints = snapshot.val();
-    });
-  }
+    }
+  );
 
+  const listRef = ref(storage, puzzleID);
+  var fireImage = [];
+
+  await listAll(listRef).then((res) => {
+    res.items.forEach((itemRef) => {
+      // All the items under listRef.
+      //console.log(itemRef + "\n");
+      fireImage.push(itemRef);
+    });
+  });
+
+  //console.log(fireImage[0]);
+  var newI = 0;
   return {
-    props: { time, getPoints, puzzleType, decrement, puzzleID }, // will be passed to the page component as props
+    props: {
+      time,
+      getPoints,
+      puzzleType,
+      decrement,
+      puzzleID,
+      fireImage,
+      newI,
+    }, // will be passed to the page component as props
   };
 }
